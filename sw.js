164eadca1f5da8bb821b82168c447e358a0eb221
abdulaziz musazay <abdulazizmusazay@abdulazizs-MacBook-Pro.local>
2026-09-10 18:41:23 -0700

    Update prototype (381a5f8b)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Bump CACHE whenever the app file changes, or phones will keep serving the
    old one. */
 
-const CACHE = "packout-24787588";
+const CACHE = "packout-381a5f8b";
 
 const SHELL = [
   "./",
